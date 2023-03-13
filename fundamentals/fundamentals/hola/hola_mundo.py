f5a4c523958f937f605df89d7450e6ce51b52c7a
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-13 11:00:08 -0300

    1

---
+++ fundamentals/fundamentals/hola/hola_mundo.py
@@ -0,0 +1,20 @@
+print("¡Hola, Mundo!")
+import random
+
+print('Bienvenido a Python')
+
+print('Este es un loop de 5 tiempos')
+for x in range(1, 6):
+    print(f'x is: {x}')
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+day = random.choice(weekdays)
+
+print(f'Today is: {day}')
+
+if day == 'Monday':
+    print('It will be a long week!')
+elif(day == 'Friday'):
+    print('Woohoo, time for the weekend!')
+else:
+    print('Not quite there yet.')
