f33f3cf332fe7a16e7a50eb363fffbb775a86134
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-13 15:25:33 -0300

    2

---
+++ fundamentals/fundamentals/hola/Hola_Mundo2.py
@@ -0,0 +1,16 @@
+print("Hola,Mundo")
+
+name = "Noelle"
+print("Hola,", name)	
+print("Hola," + name)	
+
+name2 = 42
+print("Hola", name2)	
+print("holaaaaaaaaaaaanda"+ name2)
+
+# fave_food1 = "sushi"
+# fave_food2 = "pizza"
+# print("Amo comer".format(fave_food1)) # con .format()
+# print("Amo comer" , file=(fave_food2)) # con una cadena f
+
+
