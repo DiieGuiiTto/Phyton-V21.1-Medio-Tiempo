be83e8b712830124e3415f6e6c06062053dac557
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-15 09:06:35 -0300

    tikitaka

--- fundamentals/fundamentals/hola/Hola_Mundo2.py
+++ fundamentals/fundamentals/hola/Hola_Mundo2.py
@@ -6,11 +6,9 @@ print("Hola," + name)
 
 name2 = 42
 print("Hola", name2)	
-print("holaaaaaaaaaaaanda"+ name2)
+print("holaaaaaaaaaaaanda", name2)  # Agregamos una coma después del string para concatenar con name2, que es un entero
 
-# fave_food1 = "sushi"
-# fave_food2 = "pizza"
-# print("Amo comer".format(fave_food1)) # con .format()
-# print("Amo comer" , file=(fave_food2)) # con una cadena f
-
-
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("Amo comer {}".format(fave_food1))  # Agregamos {} para reemplazar con el valor de fave_food1
+print("Amo comer", fave_food2)  # Eliminamos el argumento file ya que no es necesario y causaría un error sintáctico
