a93b3a185c301a5a9b2fd2e6fd9f10b0a0e443a6
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-15 09:11:16 -0300

    asignacion hola_mundo

--- fundamentals/fundamentals/hola/Hola_Mundo2.py
+++ fundamentals/fundamentals/hola/Hola_Mundo2.py
@@ -4,11 +4,11 @@ name = "Noelle"
 print("Hola,", name)	
 print("Hola," + name)	
 
-name2 = 42
+name2 = "42"
 print("Hola", name2)	
-print("holaaaaaaaaaaaanda", name2)  # Agregamos una coma después del string para concatenar con name2, que es un entero
+print("holaaaaaaaaaaaanda" + name2)  # Agregamos una coma después del string para concatenar con name2, que es un entero
 
 fave_food1 = "sushi"
 fave_food2 = "pizza"
-print("Amo comer {}".format(fave_food1))  # Agregamos {} para reemplazar con el valor de fave_food1
-print("Amo comer", fave_food2)  # Eliminamos el argumento file ya que no es necesario y causaría un error sintáctico
+print("Amo comer {} y {}" .format(fave_food1,fave_food2))  # Agregamos {} para reemplazar con el valor de fave_food1
+print(f"Amo comer {fave_food2} y {fave_food1}" ) # Eliminamos el argumento file ya que no es necesario y causaría un error sintáctico
