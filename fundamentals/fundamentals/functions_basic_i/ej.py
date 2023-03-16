ed6012b60bf360ee81650881b5145c0c0e2056b4
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-16 13:01:57 -0300

    2

---
+++ fundamentals/fundamentals/functions_basic_i/ej.py
@@ -0,0 +1,14 @@
+def saludame(nombre):
+    return print("Hola mundo saluda:" ,nombre)
+
+
+def operatoria(a,b,c):
+    ope = a + b - c
+    return print("el resultado es:" ,ope)
+
+
+
+x=operatoria(10,200,100)
+
+print(x)
+
