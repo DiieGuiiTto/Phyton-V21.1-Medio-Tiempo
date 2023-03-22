2e4d44c79f871df2171dff2a9c5a6b4e681d068c
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-22 11:22:45 -0300

    111

---
+++ fundamentals/fundamentals/bucle/Funciones_basicas_II.py
@@ -0,0 +1,14 @@
+def countdown(num):
+     while num >= 0:
+         print(num)
+         num -= 1
+
+countdown(5)
+#2
+def imprimir_y_devolver(lst):
+        print(lst[0])
+        return lst[5]
+
+print(imprimir_y_devolver([1,2,3,4,5,6]))
+#3
+
