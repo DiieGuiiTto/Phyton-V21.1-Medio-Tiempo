3c6438b9d9ea20d982fa8d2b8b1f42c15446d159
DiegoIS1801~ <diego.iturra.silva@alumnos.sip.cl>
2023-03-30 13:12:37 -0300

    21231231

--- POO/Cuenta_Bancaria/cuenta_bancaria.py
+++ POO/Cuenta_Bancaria/cuenta_bancaria.py
@@ -1,14 +1,13 @@
 class CuentaBancaria:
-
-    nombre_banco = "PrimerDojo"
     def __init__(self, int_rate, balance):
-        self.tasa_int = tasa_interes
+        self.tasa_int = int_rate
         self.balance = balance
-        self.name = name 
+
     def depositar(self, monto):
         self.balance += monto
-        print(f"Se Ah Depositado {monto} a {self.name} Saldo Actual: {self.balance}")
+        print(f"Se Ah Depositado {monto} Saldo Actual: {self.balance}")
         return self
+    
     def retiro(self, monto):
         if monto > self.balance:
             self.balance -=5
@@ -25,3 +24,22 @@ class CuentaBancaria:
         print(f"Tasa De Interes: {self.tasa_int}")
         print(f"Saldo Disponible: {self.balance}")
         return self
+
+class Usuario:
+
+    def __init__(self, name, email, int_tasa ,balan):
+        self.name = name
+        self.email = email
+        self.cuenta = CuentaBancaria(int_rate=int_tasa,balance=balan)
+
+
+
+
+
+
+
+diego = Usuario("Diego" ,"diego@gmail.com" ,int_tasa = 0.2 ,balan=2000 )
+cristofer = Usuario("Cristofer","cristofer@gmail.com", int_tasa= 0.2, balan=2000)
+
+diego.cuenta.depositar(1000).depositar(1000).depositar(1000).retiro(1500).info_cuenta()
+cristofer.cuenta.depositar(1000).depositar(1000).retiro(200) .retiro(200) .retiro(200) .retiro(200).info_cuenta()
